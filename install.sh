e48ccc455359141b0b51744f8363f1406bbf6211
Torben Christ <torben.christ@freudenberg.com>
2022-07-01 15:01:35 +0200

    add bash_git for container

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ export DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 
 ## if we are inside a docker container
 if [[ -f /.dockerenv ]]; then
+  curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
   cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
   cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
-  echo "source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
+  echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
 ## any else unix system
 else
   ## to get version of os
