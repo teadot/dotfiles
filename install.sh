a7b4740919d7ac89eb39691ff502984eb77b5cd2
Torben Christ <torben.christ@freudenberg.com>
2023-02-10 15:20:40 +0100

    set for alpine docker

--- install.sh
+++ install.sh
@@ -5,10 +5,26 @@ export DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 
 ## if we are inside a docker container
 if [[ -f /.dockerenv ]]; then
-  curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
-  cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
-  cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
-  echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
+  if [[ -f "/etc/os-release" ]] then 
+    . /etc/os-release
+
+    if [[ "$ID" == "alpine " ]] then
+      apk add curl
+
+      curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.git-prompt.sh
+      cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
+      echo ". ${HOME}/.git-prompt.sh; . ${HOME}/.prompt" > ${HOME}/.profile
+
+      ENV=${HOME}/.profile; export ENV
+
+    fi
+
+  fi
+
+  # curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
+  # cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
+  # cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
+  # echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
 ## any else unix system
 else
   ## to get version of os
