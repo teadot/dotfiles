a0492976ee76e75682b234c95b7977eae4ea578a
Torben Christ <torben.christ@freudenberg.com>
2023-03-24 11:53:37 +0100

    change container setup

--- install_container.sh
+++ install_container.sh
@@ -3,8 +3,25 @@
 ## export dotfiles path
 export DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-## if we are inside a docker container
-curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
-cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
-cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
-echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
+if [[ -f "/etc/os-release" ]] then 
+    . /etc/os-release
+
+    if [[ "$ID" == "alpine " ]] then
+      apk add curl
+
+      curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.git-prompt.sh
+      cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
+      echo ". ${HOME}/.git-prompt.sh; . ${HOME}/.prompt" > ${HOME}/.profile
+
+      ENV=${HOME}/.profile; export ENV
+
+    fi
+
+fi
+
+
+# ## if we are inside a docker container
+# curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
+# cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
+# cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
+# echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
