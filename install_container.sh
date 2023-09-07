8b6923c8e524b7d772d10dd0eb1331509b72fc5f
Torben Christ <torben.christ@freudenberg.com>
2023-09-07 07:07:12 +0200

    fix typo

--- install_container.sh
+++ install_container.sh
@@ -5,7 +5,6 @@ export DOTFILES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 
 FILE=/etc/os-release
 
-
 if [ -f "$FILE" ]; then 
     . $FILE
 
@@ -22,15 +21,8 @@ if [ -f "$FILE" ]; then
       curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
       cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
       cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
-      echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrcKU
+      echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
 
     fi
 
 fi
-
-
-# ## if we are inside a docker container
-# curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ${HOME}/.bash_git
-# cp ${DOTFILES_DIR}/dots/.prompt ${HOME}/.prompt
-# cp ${DOTFILES_DIR}/dots/.bash_aliases ${HOME}/.bash_aliases
-# echo "source ${HOME}/.bash_git; source ${HOME}/.prompt; source ${HOME}/.bash_aliases" > ${HOME}/.bashrc
