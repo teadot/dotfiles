2df5bec65a771e9142132c6bb81662b36ae3b8a6
Torben Christ <torben.christ@freudenberg.com>
2023-09-07 07:01:39 +0200

    change order

--- bin/az_login.sh
+++ bin/az_login.sh
@@ -10,12 +10,12 @@ fi
 source ~/bin/.env
 
 az login --tenant ${FGROUP_TENANT_ID} --output none
-az account set --subscription "az5"
-az aks get-credentials --resource-group rg_az5_aks_dev --name aks_az5_rndhub_dev --overwrite-existing
 az account set --subscription "az6"
 az aks get-credentials --resource-group rg_az6_aks_test --name aks_az6_rndhubtest_test --overwrite-existing
 az account set --subscription "az7"
 az aks get-credentials --resource-group rg_az7_aks_prod --name aks_az7_rndhubprod_prod --overwrite-existing
 az account set --subscription "fti-data-science.svc-mgmt"
 az aks get-credentials --resource-group rg_svc-mon_aks_prod --name aks_svc-mon_monitoring_prod --overwrite-existing
+az account set --subscription "az5"
+az aks get-credentials --resource-group rg_az5_aks_dev --name aks_az5_rndhub_dev --overwrite-existing
 kubelogin convert-kubeconfig -l azurecli
