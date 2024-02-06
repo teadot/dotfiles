87e84a0b68ef66ac0fbb1702badad13370a85fb2
Torben Christ <torben.christ@freudenberg.com>
2024-02-06 08:13:12 +0100

    little bit fancy output

--- bin/update_wsl.sh
+++ bin/update_wsl.sh
@@ -1,5 +1,22 @@
 sudo -v
+
+
+echo '/==============================================================================\'
+echo '||                                                                            ||'
+echo '||    ____            _                                   _       _           ||'
+echo '||   / ___| _   _ ___| |_ ___ _ __ ___    _   _ _ __   __| | __ _| |_ ___     ||'
+echo '||   \___ \| | | / __| __/ _ \ '\''_ ` _ \  | | | | '\''_ \ / _` |/ _` | __/ _ \    ||'
+echo '||    ___) | |_| \__ \ ||  __/ | | | | | | |_| | |_) | (_| | (_| | ||  __/    ||'
+echo '||   |____/ \__, |___/\__\___|_| |_| |_|  \__,_| .__/ \__,_|\__,_|\__\___|    ||'
+echo '||          |___/                              |_|                            ||'
+echo '||                                                                            ||'
+echo '\==============================================================================/'
+echo \\n
+echo "-> Update via apt"
 # update ubuntu
-sudo apt update && sudo apt upgrade -y
+sudo apt update -qq && sudo apt upgrade -qq --yes
+echo \\n
+echo "-> Update via brew"
 # update brew
-brew update && brew upgrade
+export HOMEBREW_NO_ENV_HINTS
+brew update -q && brew upgrade -q
